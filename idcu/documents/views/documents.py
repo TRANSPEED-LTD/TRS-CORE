88ef8c75cc2207bb5822aec06c7852a9b5b6d40a
Davit <davit.kv8@gmail.com>
2024-06-17 00:09:46 +0400

    add single order get view

--- idcu/documents/views/documents.py
+++ idcu/documents/views/documents.py
@@ -3,7 +3,7 @@ from typing import Any
 from base_idcu.views.base import IDCUView
 from documents.views.base import BaseDocumentView
 from documents.serializers.output import OrderResponse
-from documents.serializers.input import OrderToCreate, OrderToFetch
+from documents.serializers.input import OrderToCreate, OrdersToFetch, OrderToFetch
 
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
@@ -37,7 +37,7 @@ class OrdersView(BaseDocumentView, IDCUView):
     """Handles request to the `company/<str:get-orders>/` endpoint."""
 
     http_method_names = ['get']
-    in_serializer_cls = OrderToFetch
+    in_serializer_cls = OrdersToFetch
 
     def process_request(self, request_params: Any) -> OrderResponse:
         """
@@ -50,5 +50,27 @@ class OrdersView(BaseDocumentView, IDCUView):
         """
         user = self.request.user
         response_data = self.service_class.fetch_orders_for_company(company=user.company)
+
+        return OrderResponse(response_data, many=True).data
+
+
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+class OrderView(BaseDocumentView, IDCUView):
+    """Handles request to the `company/<str:get-orders>/` endpoint."""
+
+    http_method_names = ['get']
+    in_serializer_cls = OrderToFetch
+
+    def process_request(self, request_params: Any) -> OrderResponse:
+        """
+        process request for `company/get-order/` endpoint.
+
+        Fetches specific order for requested order id.
+
+        :param request_params: Request parameters.
+        :return: Serialized response.
+        """
+        response_data = self.service_class.fetch_order_by_id(order_id=request_params["order_id"])
 
         return OrderResponse(response_data, many=True).data

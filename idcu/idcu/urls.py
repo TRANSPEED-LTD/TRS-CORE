8667b23517af247f3b6c1ef7b9348d0016ba33e8
Davit <davit.kv8@gmail.com>
2025-04-02 14:01:08 +0400

    Add health check path

--- idcu/idcu/urls.py
+++ idcu/idcu/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +9,8 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('documents/', include('documents.urls'))
 ]
+
+if settings.DEBUG is False:
+    urlpatterns += [
+        path("health/", lambda r: HttpResponse("OK"), name="health"),
+    ]
